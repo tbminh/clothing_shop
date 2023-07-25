edf7acba854492e7073799c2212f96308922bf62
dinhlamhuy <dinhlamhuytak489@gmail.com>
2023-07-26 01:21:45 +0700

    update giao dien auth

--- resources/views/components/TheFooter.vue
+++ resources/views/components/TheFooter.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="container-fluid bg-dark text-white">
-        <div class="container border">
+        <div class="container ">
             <div class="row">
-                <div class="col-4 p-3 border d-flex align-items-center">
-                    Logo <br />
-                    Địa chỉ: Ấp Tân Hòa B xã Long Hưng, Huyện Mỹ Tú, Tỉnh Sóc Trăng <br />
+                <div class="col-4 p-2  d-flex flex-wrap align-items-center">
+                    <span class="fs-5 fw-bold">Logoo</span>
+                    Địa chỉ: Hẻm 48 phường bla bak, Quận bal bak, Tỉnh haha <br />
                     0123-456-789<br />
                     Email: demo@example.com
                 </div>
-                <div class="col-4 p-4 border">
+                <div class="col-4 p-2 ">
                     <div class="d-flex flex-wrap align-items-center">
                         <span class="fs-5 fw-bold">Shopping & Categories</span>
                     </div>
@@ -23,10 +23,10 @@
                     </div>
                 </div>
 
-                <div class="col-4 p-3 border d-flex align-items-center">
+                <div class="col-4 py-3  d-flex align-items-center">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.7508082085023!2d105.725856874196!3d9.954679190148413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a08bceb0739df7%3A0xa59f359f4e53cee7!2sLACTY2%20COMPANY!5e0!3m2!1svi!2s!4v1690289570479!5m2!1svi!2s"
-                        width="450" height="150" style="border:0;" loading="lazy"
+                        width="100%" height="100%" style="border:0;" loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
@@ -37,4 +37,5 @@
             </div>
 
         </div>
-</div></template>
+    </div>
+</template>
